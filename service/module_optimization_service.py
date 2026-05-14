050c2eaab42dd62b8c40de5b84ccd511ef85a111
Yunseong <dev.yunseong@gmail.com>
2026-05-15 00:07:17 +0900

    feat: add RAW_SA_OPTIMIZATION_SERVICE to module optimization service

--- service/module_optimization_service.py
+++ service/module_optimization_service.py
@@ -1,11 +1,14 @@
 from mr2s_module import (
   QuboMR2SSolver,
-  SAQuboSolver,
-  SmallWorldSpec,
+  SAMR2SSolver
 )
 
 from service.optimization_service import ProxyModuleOptimizationService
 
 NONE_FACE_CYCLE_OPTIMIZATION_SERVICE = ProxyModuleOptimizationService(
   QuboMR2SSolver()
+)
+
+RAW_SA_OPTIMIZATION_SERVICE = ProxyModuleOptimizationService(
+  SAMR2SSolver()
 )
